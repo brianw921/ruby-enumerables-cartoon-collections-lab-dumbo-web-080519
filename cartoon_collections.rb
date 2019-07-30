66904e20000fdd5ee822ee00eaf37b0f2c536067
Brian Wong <bwong92@gmail.com>
2019-07-30 07:15:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,11 @@ end
 
 def summon_captain_planet(array)
   # Your code here
+  new_arr
   array.each do |w|
-
+    new_arr << w + "!"
   end
+  new_arr
 end
 
 def long_planeteer_calls# code an argument here
