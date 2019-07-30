5795bd9b46a9b5653f293d694c523bb24d2be7e7
Brian Wong <bwong92@gmail.com>
2019-07-30 07:17:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def summon_captain_planet(array)
   new_arr
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
